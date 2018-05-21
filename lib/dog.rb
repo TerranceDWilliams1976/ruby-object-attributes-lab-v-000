8929ae69e8a49d19d0bf7ba60060484ac4644a3e
Terrance Williams <terrance.d.williams1976@gmail.com>
2018-05-21 03:25:25 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,13 +2,12 @@ class Dog
   def name(dog_name)
     @dogs_name = dog_name
 end
+
   def breed(breed)
     @dog_breed = breed
 end
 
-  def name=(name)
-    @name = breed
-end
+
 
   def breed=(
     @breed
